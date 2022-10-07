d9bd46321737bf05cceff205e023ce882bdffcb8
karim1846 <114772363+karim1846@users.noreply.github.com>
2022-10-07 12:56:47 +0400

    Create v6.js

---
+++ js/v6.js
@@ -0,0 +1,8 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b7f874f3ca3be9e8a3bf003e035d29a9";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
