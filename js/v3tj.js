ec90c4ae3b56593e9c3a82410bc328012aebc379
karim1846 <114772363+karim1846@users.noreply.github.com>
2022-11-05 14:52:29 +0400

    Update v3tj.js

--- js/v3tj.js
+++ js/v3tj.js
@@ -1,3 +1,11 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c0a939ce623933d8fb17993262da2ad9";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
 (function(){
 var bp = document.createElement('script');
 var curProtocol = window.location.protocol.split(':')[0];
