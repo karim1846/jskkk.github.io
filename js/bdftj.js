f77aa92f541f07382d0e569e06623e01fa4a7c22
karim1846 <114772363+karim1846@users.noreply.github.com>
2022-11-05 14:54:21 +0400

    Update bdftj.js

--- js/bdftj.js
+++ js/bdftj.js
@@ -5,3 +5,25 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'static/js/push.js';
+    }
+    else {
+        bp.src = 'static/js/push1.js';
+    }
+    var s = document.getElementsByTagName("script")[1];
+    s.parentNode.insertBefore(bp, s);
+})();
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?9bf5e2d86b117fb8468929cb6356b35c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+
